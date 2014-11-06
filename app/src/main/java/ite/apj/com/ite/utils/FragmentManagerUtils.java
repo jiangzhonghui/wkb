16bca5c7481c20d1dfa7a811853f1b701a1b02d2
huangbo <877406173@qq.com>
2014-11-06 17:42:40 +0800

    test_huangbo

---
+++ app/src/main/java/ite/apj/com/ite/utils/FragmentManagerUtils.java
@@ -0,0 +1,13 @@
+package ite.apj.com.ite.utils;
+
+
+
+/**
+ * Created by student on 2014/11/6.
+ */
+public class FragmentManagerUtils {
+
+    private static final String ARG_SECTION_NUMBER ="section_number";
+
+
+}
