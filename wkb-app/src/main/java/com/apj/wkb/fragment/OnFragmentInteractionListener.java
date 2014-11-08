40b5b29e5f6351e43439ea62fb3e0f647bdc3812
JiangZhonghui <timo.jiang@qq.com>
2014-11-08 16:11:04 +0800

    Add home activity and home tabs

---
+++ wkb-app/src/main/java/com/apj/wkb/fragment/OnFragmentInteractionListener.java
@@ -0,0 +1,8 @@
+package com.apj.wkb.fragment;
+
+import android.net.Uri;
+
+public interface OnFragmentInteractionListener {
+    // TODO: Update argument type and name
+    public void onFragmentInteraction(Uri uri);
+}
