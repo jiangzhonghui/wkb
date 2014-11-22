c7206ba046d18d2e36359f61c4876a613e09c9f3
JiangZhonghui <timo.jiang@qq.com>
2014-11-22 15:10:21 +0800

    Update

--- wkb-app/src/main/java/com/apj/wkb/HomeFragment/HomeFragment.java
+++ wkb-app/src/main/java/com/apj/wkb/HomeFragment/HomeFragment.java
@@ -1,35 +1,49 @@
 package com.apj.wkb.HomeFragment;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.Loader;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.apj.wkb.OnFragmentInteractionListener;
 import com.apj.wkb.R;
+import com.apj.wkb.adapter.ImageBannerPagerAdapter;
+import com.apj.wkb.entity.CourserItem;
+import com.apj.wkb.entity.HomeCategory;
+import com.apj.wkb.loader.HomeCategoryLoader;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Fragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Fragment#newInstance} factory method to
- * create an instanc this fragment.
- *
- */
-public class HomeFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<HomeCategory>>{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+    private RelativeLayout gllery_container;
+    private RelativeLayout empty_view_for_you;
+    private ViewPager mViewPager;
+    private ProgressBar recommend_loading;
+    private TextView recommend_no_data;
+    private GridView grid_view_for_you;
+    private TextView no_data_text_for_you;
+    private ProgressBar  for_you_loading;
+    private List<CourserItem> topData;
     private String mParam1;
     private String mParam2;
-
+    private ImageBannerPagerAdapter topAdapter;
     private OnFragmentInteractionListener mListener;
 
     /**
@@ -60,16 +74,33 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        mViewPager = (ViewPager)this.getView().findViewById(R.id.gallery);
+        gllery_container=(RelativeLayout)this.getView().findViewById(R.id.gllery_container);
+        recommend_loading = (ProgressBar)this.getView().findViewById(R.id.recommend_loading);
+        recommend_no_data = (TextView)this.getView().findViewById(R.id.recommend_no_data);
+        grid_view_for_you = (GridView)this.getView().findViewById(R.id.grid_view_for_you);
+        no_data_text_for_you = (TextView)this.getView().findViewById(R.id.no_data_text_for_you);
+        for_you_loading = (ProgressBar)this.getView().findViewById(R.id.for_you_loading);
+        empty_view_for_you =(RelativeLayout)this.getView().findViewById(R.id.empty_view_for_you);
+        gllery_container.setVisibility(View.GONE);
+        recommend_loading.setVisibility(View.VISIBLE);
+        grid_view_for_you.setVisibility(View.GONE);
+        empty_view_for_you.setVisibility(View.GONE);
+
+        topData = new ArrayList<CourserItem>();
+        topAdapter = new ImageBannerPagerAdapter(this.getActivity(),topData);
+        mViewPager.setAdapter(topAdapter);
+
+        getLoaderManager().initLoader(0,null,this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.frag_hm_main, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -93,5 +124,33 @@ public class HomeFragment extends Fragment {
         mListener = null;
     }
 
+    @Override
+    public android.support.v4.content.Loader<List<HomeCategory>> onCreateLoader(int i, Bundle bundle) {
+        return new HomeCategoryLoader(this.getActivity());
+    }
 
+    @Override
+    public void onLoadFinished(android.support.v4.content.Loader<List<HomeCategory>> listLoader, List<HomeCategory> homeCategories) {
+        if(homeCategories!=null && homeCategories.size()>0){
+            gllery_container.setVisibility(View.VISIBLE);
+            recommend_loading.setVisibility(View.GONE);
+            grid_view_for_you.setVisibility(View.VISIBLE);
+            empty_view_for_you.setVisibility(View.GONE);
+            for(HomeCategory item:homeCategories){
+                if(item.getType().equals("0")){
+                    topData.clear();
+                    topData.addAll(item.getVos());
+                    topAdapter.notifyDataSetChanged();
+                }
+            }
+        }else{
+            recommend_loading.setVisibility(View.GONE);
+            recommend_no_data.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(android.support.v4.content.Loader<List<HomeCategory>> listLoader) {
+
+    }
 }
---
+++ wkb-app/src/main/java/com/apj/wkb/adapter/ImageBannerPagerAdapter.java
@@ -0,0 +1,57 @@
+package com.apj.wkb.adapter;
+import android.app.Activity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.apj.wkb.entity.CourserItem;
+import com.apj.wkb.entity.HomeCategory;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ImageBannerPagerAdapter extends PagerAdapter {
+
+    private Activity context;
+    private List<CourserItem> topBannerData;
+    private boolean isDownloadImageIn3G;
+    int w =0 ;//500
+    int h =0 ;//220
+
+    public ImageBannerPagerAdapter(Activity context, List<CourserItem> imageList) {
+        this.context = context;
+        this.topBannerData = imageList;
+    }
+    @Override
+    public int getCount() {
+        return topBannerData.size();
+    }
+
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view.equals(object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        CourserItem entity = topBannerData.get(position);
+        //    ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(w, ViewGroup.LayoutParams.WRAP_CONTENT);
+        //NetworkImageView imageView = new NetworkImageView(context);
+        final ImageView imageView = new ImageView(context);
+        String imgUrl = entity.getContentUrl();
+        imageView.setTag(imgUrl);
+        Picasso.with(context).load(imgUrl).fit().into(imageView);//.resize( w,h)
+        ((ViewPager) container).addView(imageView, 0);
+        return imageView;
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ((ViewPager) container).removeView((ImageView) object);
+    }
+}
+
